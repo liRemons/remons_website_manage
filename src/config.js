692a6283343dc85db42c747868baec24d056475f
Remons <remons@foxmail.com>
2026-05-07 10:15:51 +0800

    Update production HOST_URL to new IP address

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const HOST_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:3009'
-    : 'http://8.136.206.131:3009';
+    : 'http://120.55.247.73:3009';
 
 export const DES_KEY = 'remons';
 
