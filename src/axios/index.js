e6bc118f66cc4e9a46ba2a01807296ea9e604145
liRemons <remons@foxmail.com>
2026-06-03 21:51:59 +0800

    feat: test

--- src/axios/index.js
+++ src/axios/index.js
@@ -62,7 +62,6 @@ arr.forEach((item) => {
       if (!res.success) {
         message.error(res.message || res.msg);
         if (res.code === 401) {
-          console.log(res, '====');
           window.location.href = `/manage/login?from=${window.location.href}`;
         }
 
